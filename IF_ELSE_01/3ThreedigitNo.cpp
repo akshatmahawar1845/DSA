17b9252f0f9e141f7d90fa6549c8bbdbb9c7bad5
akshatmahawar1845 <akshatmahawar18@gmail.com>
2026-07-02 17:07:56 +0530

    Three digit number or not

---
+++ IF_ELSE_01/3ThreedigitNo.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter a number :";
+    cin >> n;
+    if(n>99 && n<=999) cout <<"Three digit number";
+    else cout <<"Not a three digit number";
+}
