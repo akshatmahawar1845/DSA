dab06031392703937c178538d8a5049679488204
akshatmahawar1845 <akshatmahawar18@gmail.com>
2026-07-02 16:59:02 +0530

    Even odd number

---
+++ IF_ELSE_01/1Even.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "ENTER A NUMBER :";
+    cin >> n;
+    if(n%2==0) cout << "Even number";
+    else cout << "Odd number";
+}
