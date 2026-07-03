2a2abaf33e0a328edd825367dba7dcccf975aa99
akshatmahawar1845 <akshatmahawar18@gmail.com>
2026-07-03 22:32:33 +0530

    Least of three number

---
+++ IF_ELSE_01/LeastOfThree.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+       int a,b,c;
+    cout << "Enter first number :";
+    cin >> a;
+
+    cout << "Enter second number :";
+    cin >> b;
+
+    cout << "Enter third number :";
+    cin >> c;
+
+    if(a<=b && b<=c) cout << a << " is least";
+    else if(b<=a && b<=c) cout << b << "is least";
+    else cout << c << " is least";
+}
