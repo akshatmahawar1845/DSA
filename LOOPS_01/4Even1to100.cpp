ee152e9fcd29c9d87797f3aba4510a151700854b
akshatmahawar1845 <akshatmahawar18@gmail.com>
2026-07-04 22:59:31 +0530

    even number with 2nd method

--- LOOPS_01/4Even1to100.cpp
+++ LOOPS_01/4Even1to100.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    for(int i = 1; i <= 100; i++){
-        if(i%2==0) cout << i << endl;
+    // for(int i = 1; i <= 100; i++){   
+    //     if(i%2==0) cout << i << endl;
+    // }    using if statement
+
+    for(int i = 1; i <= 100; i+=2){  // using +2 increment
+        cout << i << endl;
     }
 }
