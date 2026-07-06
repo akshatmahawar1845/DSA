eae1c00c5853acc95d93327ec9558a07182b0fbe
akshatmahawar1845 <akshatmahawar18@gmail.com>
2026-07-06 11:46:31 +0530

    Prime number

---
+++ LOOPS_02/2Prime.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter a number :";
+    cin >> n;
+    for(int i = 2 ; i <= sqrt(n); i++){
+        if(n%i!=0){
+            cout <<"Prime number";
+            break;
+        }
+    }
+}
