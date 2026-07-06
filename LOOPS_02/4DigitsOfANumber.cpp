e8aba2f9cfbf74f9588f05abb20fb38441a46fae
akshatmahawar1845 <akshatmahawar18@gmail.com>
2026-07-06 12:31:55 +0530

    Modify code for 0

--- LOOPS_02/4DigitsOfANumber.cpp
+++ LOOPS_02/4DigitsOfANumber.cpp
@@ -4,10 +4,13 @@ int main(){
     int n;
     cout << "Enter a number :";
     cin >> n;
-    int count = 0;
-    while(n>0){
+    int count;
+    if(n==0) count = 1;
+    else count = 0;
+    while(n!=0){
         n /= 10;
         count++;
     }
+    // if(n==0) cout <<"1";
     cout << count;
 }
