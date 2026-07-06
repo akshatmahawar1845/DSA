ca0975e79e4cba28ab2094afc15c721b64958940
akshatmahawar1845 <akshatmahawar18@gmail.com>
2026-07-06 11:30:46 +0530

    Gp code

---
+++ LOOPS_01/7Gp.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter a number :";
+    cin >> n; // gp = 1,2,4,8,16
+    int a = 1;
+    for(int i = 1; i <= n; i++){
+        cout <<a << " ";
+        a *= 2;
+    }
+}
