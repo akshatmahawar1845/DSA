be5baa7ab2fdc4c354019a9dccfb641eb426379f
akshatmahawar1845 <akshatmahawar18@gmail.com>
2026-07-06 12:38:22 +0530

    Modify sum of digits of a no. for negative number

--- LOOPS_02/5SumOfDigits.cpp
+++ LOOPS_02/5SumOfDigits.cpp
@@ -4,8 +4,9 @@ int main(){
     int n;
     cout << "Enter a number :";
     cin >> n;
+    if(n<0) n *= -1;
     int sum = 0;
-    while(n>0){
+    while(n!=0){
        sum += (n%10);
        n /= 10;
     }
