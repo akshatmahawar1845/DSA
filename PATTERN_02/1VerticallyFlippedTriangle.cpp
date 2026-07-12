8dcbb015fa91b12ab1212492c73bd2c1a9ef4189
akshatmahawar1845 <akshatmahawar18@gmail.com>
2026-07-12 23:28:35 +0530

    Method 2 for VF  triangle

--- PATTERN_02/1VerticallyFlippedTriangle.cpp
+++ PATTERN_02/1VerticallyFlippedTriangle.cpp
@@ -4,11 +4,23 @@ int main(){
     int n;
     cout << "Enter number of rows :";
     cin >> n;
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= n; j++){
-            if((i + j) <= n) cout << "  ";
-            else cout << "* ";
+    // METHOD-1
+    // for(int i = 1; i <= n; i++){
+    //     for(int j = 1; j <= n; j++){
+    //         if((i + j) <= n) cout << "  ";
+    //         else cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // METHOD-2
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n-i; j++){
+                cout << "  ";
+            }
+            for(int j = 1; j <= i; j++){
+                cout << "* ";
+            }
+            cout << endl;
         }
-        cout << endl;
-    }
 }
