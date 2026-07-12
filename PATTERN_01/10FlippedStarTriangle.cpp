a59bed9ee6b030ef4e95bf00ce868f3051bff6a9
akshatmahawar1845 <akshatmahawar18@gmail.com>
2026-07-12 23:06:40 +0530

    Modify Code

--- PATTERN_01/10FlippedStarTriangle.cpp
+++ PATTERN_01/10FlippedStarTriangle.cpp
@@ -1,11 +1,23 @@
 #include<iostream>
 using namespace std;
 int main(){
+
+    
     int n;
     cout << "Enter number of rows :";
     cin >> n;
-    for(int i = n; i >= 1; i--){
-        for(int j = 1; j <= i ; j++){
+    // METHOD-1
+    // for(int i = n; i >= 1; i--){
+    //     for(int j = 1; j <= i ; j++){
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
+
+
+    // METHOD-2
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n+1-i; j++){
             cout << "* ";
         }
         cout << endl;
